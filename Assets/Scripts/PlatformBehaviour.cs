818e9b8c18d2f0a19ff421a89b893c92aab9c9c9
Jordi <xbarciap@gmail.com>
2022-05-13 19:04:07 +0200

    Level + Platform

---
+++ Assets/Scripts/PlatformBehaviour.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour : MonoBehaviour
+{
+	private PlatformEffector2D effector;
+	public float waitTime;
+	public float moveInput;
+	public bool isPlayer;
+	// Start is called before the first frame update
+	void Start()
+	{
+		effector = GetComponent<PlatformEffector2D>();
+		isPlayer = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		moveInput = Input.GetAxisRaw("Vertical");
+
+		if (moveInput < 0 && isPlayer == true)
+		{
+			effector.rotationalOffset = 180f;
+			waitTime -= Time.deltaTime;
+		}
+		if (Input.GetButtonDown("AltJump") || waitTime <= 0)
+		{
+			effector.rotationalOffset = 0;
+			waitTime = 0.5f;
+		}
+	}
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag != "Player")
+		{
+			effector.rotationalOffset = 0;
+		}
+		else if (collision.tag == "Player")
+		{
+			isPlayer = true;
+		}
+	}
+}
---
+++ Assets/Scripts/WormAI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormAI : MonoBehaviour
+{
+	public float speed;
+	public float lineOfSight = 5;
+	public bool isChasing;
+	private GameObject player;
+	private Vector2 number;
+	private float movingTime = 2;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	void Update()
+	{
+		if (movingTime <= 0)
+		{
+			number.x = Random.Range(-20, 20);
+			number.y = 0;
+			movingTime = 2;
+		}
+		else movingTime -= Time.deltaTime;
+
+		Patroling();
+
+		Flip();
+		
+	}
+	private void Patroling()
+	{
+		Vector2 moveTo = number;
+		transform.position = Vector2.MoveTowards(transform.position, moveTo, speed * Time.deltaTime);
+		Debug.Log("Worm" + Vector2.MoveTowards(transform.position, moveTo, speed * Time.deltaTime));
+	}
+	private void Flip()
+	{
+		if (transform.position.x > player.transform.position.x)
+		{
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+		else
+		{
+			transform.rotation = Quaternion.Euler(0, 180, 0);
+		}
+	}
+	
+}
