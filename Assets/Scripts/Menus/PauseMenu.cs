b8a99ca5400ab3d2061cf743a0f5f3a9bc4e2d87
Jordi <xbarciap@gmail.com>
2022-05-13 14:46:02 +0200

    Death Menu

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -1,53 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
+	public static bool GameIsPaused = false;
 
-    public GameObject pauseMenuUI;
-    public GameObject HUD;
+	public GameObject pauseMenuUI;
+	public GameObject HUD;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else 
-            {
-                Pause();
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (GameIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+	}
 
-    public void Resume() 
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-    }
+	public void Resume()
+	{
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
 
-    void Pause() 
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
-    }
+	void Pause()
+	{
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
 
-    public void LoadMenu() 
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
+	public void LoadMenu()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	}
 
-    public void QuitGame() 
-    {
-        Application.Quit();
-    }
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
 }
---
+++ Assets/Scripts/Menus/RestartMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartMenu : MonoBehaviour
+{
+	public GameObject restartMenuUi;
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.K))
+		{
+			Time.timeScale = 0f;
+			restartMenuUi.SetActive(true);
+		}
+
+	}
+	public void RestartGame()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("SampleScene");
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
