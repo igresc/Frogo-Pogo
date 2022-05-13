f77eed84d983d3c4f158daaa867bc1a280581f2b
Jordi <xbarciap@gmail.com>
2022-05-13 11:38:43 +0200

    Parry WIP

---
+++ Assets/Scripts/Parry.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parry : MonoBehaviour
+{
+	public CharacterController2D controller;
+	public bool isParrying;
+	public float slowDownTime = 0.3f;
+	public Vector2 impulseForce;
+	
+	float defaultTimeFixedValue;
+	// Start is called before the first frame update
+	void Start()
+	{
+		defaultTimeFixedValue = Time.fixedDeltaTime;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Enemy"))
+		{
+			isParrying = true;
+			Time.timeScale = slowDownTime;
+			Time.fixedDeltaTime = Time.timeScale * 0.02f;
+			if (isParrying)
+				Parry_Time();
+			
+			else {
+				//Take_Damage();
+			}
+		}
+		else if (collision.CompareTag("Parryable")) 
+		{
+			isParrying = true;
+			Time.timeScale = slowDownTime;
+			Time.fixedDeltaTime = Time.timeScale * 0.02f;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		isParrying = false;
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = defaultTimeFixedValue;
+	}
+	
+	public void Parry_Time()
+	{
+		Debug.Log(isParrying);
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = defaultTimeFixedValue;
+		
+		//if (controller.m_FacingRight)
+		//{
+		//	impulseForce.x *= 1;
+		//}
+		//else
+		//	impulseForce.x *= -1;
+
+		GetComponent<Rigidbody2D>().AddForce(impulseForce, ForceMode2D.Impulse);
+		
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Parry parryController;
 
     public float runSpeed = 40f;
     float horizontalMove = 0f;
 
     bool isJumping;
+    bool canParry;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("AltJump")) {
             isJumping = true;
+        }
+        if (Input.GetButtonDown("Jump") && parryController.isParrying) 
+        {
+            parryController.Parry_Time();
         }
     }
     void FixedUpdate()
