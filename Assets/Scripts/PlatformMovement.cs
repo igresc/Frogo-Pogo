3138a0bdff0b423fc011039abaff236a9bae1882
Jordi <xbarciap@gmail.com>
2022-05-15 16:47:49 +0200

    New Platforms

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Transform[] moveSpot;
+	private int i = 0;
+	private float waitTime;
+	public float startWaitTime = 2;
+	private GameObject Player;
+	public float speed = 1f;
+	// Start is called before the first frame update
+	private void Start()
+	{
+		Player = GameObject.FindGameObjectWithTag("Player");
+		waitTime = startWaitTime;
+	}
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
-        
-    }
+		transform.position = Vector2.MoveTowards(transform.position, moveSpot[i].transform.position, speed * Time.deltaTime);
+		if (Vector2.Distance(transform.position, moveSpot[i].transform.position) < 0.1f)
+		{
+			if (waitTime <= 0)
+			{
+				if (moveSpot[i] != moveSpot[moveSpot.Length - 1])
+				{
+					i++;
+				}
+				else
+				{
+					i = 0;
+				}
+
+				waitTime = startWaitTime;
+			}
+			else
+			{
+				waitTime -= Time.deltaTime;
+			}
+		}
+	}
 }
